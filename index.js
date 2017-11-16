d00f178e9cc03bc3c0cb4bb529e73a1a9d4cf0d3
Anthony Catalfo <anthonycatalfo@gmail.com>
2017-11-16 04:03:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@
 function printBadges(employeeNames) {
   employeeNames.map((employeeName,index,employeeNames)=>{
     console.log(`Welcome ${employeeNames[index]}! You are employee #${index+1}.`);
-    return employeeNames[];
+    return employeeNames.employeeName;
   } )
 }
